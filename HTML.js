dd349e1433948eacf9494e808def19b1ecea1332
Bofamily <familytbh6+githubbofamily@gmail.com>
2025-05-03 07:52:12 +0100

    Fixes

--- HTML.js
+++ HTML.js
@@ -11,17 +11,23 @@ function langSelection(value) {
         document.getElementById("button1000").innerHTML = "BUTTON";
         document.getElementById("button2000").innerHTML = "Reset";
         document.getElementById("langdropdown").innerHTML = "Choose a language:";
+        document.getElementById("button1").innerHTML = "Search";
+        document.getElementById("input1").placeholder = "Search Google";
     } else if (value === "option2") {
         console.log("Option 2 selected");
         document.getElementById("header").innerHTML = "JUSTE VIBE";
         document.getElementById("button1000").innerHTML = "BOUTON";
         document.getElementById("button2000").innerHTML = "Réinitialiser";
-        document.getElementById("langdropdown").innerHTML = "Choisissez une langue:"; 
+        document.getElementById("langdropdown").innerHTML = "Choisissez une langue:";
+        document.getElementById("button1").innerHTML = "Rechercher";
+        document.getElementById("input1").placeholder = "Rechercher Google";
     } else if (value === "option3") {
         console.log("Option 3 selected");
         document.getElementById("header").innerHTML = "EINFACH VIBE";
         document.getElementById("button1000").innerHTML = "KNÖPFEN";
         document.getElementById("button2000").innerHTML = "Zurücksetzen";
         document.getElementById("langdropdown").innerHTML = "Sprache wählen:";
+        document.getElementById("button1").innerHTML = "Suchen";
+        document.getElementById("input1").placeholder = "Google durchsuchen";
     }
 }
