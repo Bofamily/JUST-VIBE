259901ee9f9aa100abdce952e45aa3b231458905
Bofamily <familytbh6+githubbofamily@gmail.com>
2025-05-03 17:07:17 +0100

    Commit

--- HTML.js
+++ HTML.js
@@ -12,7 +12,10 @@ function langSelection(value) {
         document.getElementById("button2000").innerHTML = "Reset";
         document.getElementById("langdropdown").innerHTML = "Choose a language:";
         document.getElementById("button1").innerHTML = "Search";
+        document.getElementById("button2").innerHTML = "Claim Certificate";
         document.getElementById("input1").placeholder = "Search Google";
+        document.getElementById("eastereggtext1").innerHTML = "You Clicked on Nyan cat";
+        document.getElementById("eastereggtext2").innerHTML = "You Clicked on the hovering cats";
     } else if (value === "option2") {
         console.log("Option 2 selected");
         document.getElementById("header").innerHTML = "JUSTE VIBE";
@@ -21,6 +24,9 @@ function langSelection(value) {
         document.getElementById("langdropdown").innerHTML = "Choisissez une langue:";
         document.getElementById("button1").innerHTML = "Rechercher";
         document.getElementById("input1").placeholder = "Rechercher Google";
+        document.getElementById("eastereggtext2").innerHTML = "Vous avez cliqué sur les chats en vol stationnaire";
+        document.getElementById("eastereggtext1").innerHTML = "Vous avez cliqué sur Nyan cat";
+        document.getElementById("button2").innerHTML = "Certificat de réclamation";
     } else if (value === "option3") {
         console.log("Option 3 selected");
         document.getElementById("header").innerHTML = "EINFACH VIBE";
@@ -29,6 +35,10 @@ function langSelection(value) {
         document.getElementById("langdropdown").innerHTML = "Sprache wählen:";
         document.getElementById("button1").innerHTML = "Suchen";
         document.getElementById("input1").placeholder = "Google durchsuchen";
+        document.getElementById("eastereggtext1").innerHTML = "Du hast auf Nyan Katze geklicktt"
+        document.getElementById("eastereggtext2").innerHTML = "Du hast auf die schwebenden Katzen geklickt";
+        document.getElementById("button2").innerHTML = "Anspruchszertifikat";
+
         
     }
 }
